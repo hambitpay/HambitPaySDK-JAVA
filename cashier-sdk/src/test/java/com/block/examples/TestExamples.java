cbc00f20ea51f9e269a1c8b552d77e6d454dc790
hambitpay <93760875+hambitpay@users.noreply.github.com>
2021-11-05 16:04:04 +0800

    Update TestExamples.java

--- cashier-sdk/src/test/java/com/block/examples/TestExamples.java
+++ cashier-sdk/src/test/java/com/block/examples/TestExamples.java
@@ -21,35 +21,6 @@ public class TestExamples {
                 .accessKey(Constants.ACCESSKEY)
                 .secretKey(Constants.SECRETKEY)
                 .build());
-//        SellerAccountVo sellerAccountVo = cashierService.getSellerAccountVo();
-//        System.out.println(sellerAccountVo);
-//        ProductInfoAddDto addDto = new ProductInfoAddDto();
-//        addDto.setProductsAmount(100.00);
-//        addDto.setRemark("测试收款单创建");
-//        addDto.setExternalOrderId("abc-123-123");
-//        addDto.setStoreName("宇宙商店");
-//        List<Goods> goodsList = new ArrayList<>();
-//        Goods goods = new Goods();
-//        goods.setGoodsName("iphone13");
-//        goods.setGoodsSize(100);
-//        goods.setGoodsPrice(100.00);
-//        goodsList.add(goods);
-//        addDto.setGoodsList(goodsList);
-//        ProductsInfoVo vo = cashierService.addProducts(addDto);
-//        System.out.println(vo);
-
-//        List<ProductsInfoVo> voList = cashierService.listProducts(null);
-//        System.out.println(voList);
-//USDT(0,"USDT","USDT"),
-//    CNY(1,"人民币","CNY"),
-//    USD(2,"美元","USD"),
-//        RechargeAddDto dto = new RechargeAddDto();
-//        dto.setAssetType(102);
-//        dto.setTokenType(901);
-//        dto.setAmount(100.00);
-//        dto.setType(0);
-//        dto.setExternalOrderId("123131313");
-//        cashierService.addRecharge(dto);
         ProductsInfoQueryDto dto = new ProductsInfoQueryDto();
         dto.setExternalOrderId("abc-123-123");
         dto.setProductId(123131313L);
